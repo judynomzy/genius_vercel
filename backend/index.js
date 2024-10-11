1825225fc3e41229980e2e6f315790e5e65d2472
Judith Richard <judithchard1111@gmail.com>
2024-10-11 17:29:32 +0100

    express

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,9 @@ require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
 
-
+const app = express();
 app.use(cors({
     origin : process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials : true
 }))
 app.use(express.json())
